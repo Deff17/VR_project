05d14732ada1b8112ac518447adf0b4a6c0001b0
Dawid Prokopek <dawid.prokopek@allegro.pl>
2020-02-01 16:25:41 +0100

    first commit

---
+++ New Unity Project/Assets/PhisicsSimmulation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhisicsSimmulation : MonoBehaviour
+{
+
+    public int maxIteration = 1000;
+
+    [ContextMenu("Run Simulation")]
+    public void RunSimmulation()
+    {
+
+        Physics.autoSimulation = false;
+        for (int i =0; i < maxIteration; i++)
+        {
+            Physics.Simulate(Time.fixedDeltaTime);
+        }
+        Physics.autoSimulation = true;
+        
+    }
+}
